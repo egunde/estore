71899b7684729ff49200260507807d282f64c780
egunde <112527936+egunde@users.noreply.github.com>
2023-03-16 17:21:29 -0500

    no longer configured as single page

--- src/utils/firebase/firebaseInit.ts
+++ src/utils/firebase/firebaseInit.ts
@@ -9,7 +9,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
   }
-console.log(firebaseConfig)
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 export default app
