e4a480f37d91fc5b0e0224287481891d0d43702b
egunde <112527936+egunde@users.noreply.github.com>
2023-03-16 20:41:43 -0500

    added pagination to images

--- src/routes/catalog/ProductCard.tsx
+++ src/routes/catalog/ProductCard.tsx
@@ -1,28 +1,45 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Pagination, Stack, Typography } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 
 
 export default function ProductCard(props: {product: ShopifyBuy.Product}) {
+    const [page, setPage] = useState(1)
     //used to create Link
     const id = props.product.id.toString().substring(22)
     //this error is okay
     //@ts-ignore
     const price = formatCurrency(props.product.variants.at(0)?.price.amount)
 
+    const handlePage = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+        console.log(value)
+    };
+
     return (
         <Box width="30vh">
             <Card>
                 <CardMedia
-                    component="img"
-                    alt="green iguana"
-                    image={props.product.images.at(0)?.src}
-                    sx={{
-                        height: "30vh",
-                        width: "30vh"
-                    }}
-                />
+                    component="div"
+                >
+                    <Box
+                        component="img"
+                        alt={props.product.title}
+                        src={props.product.images.at(page-1)?.src}
+                        sx={{
+                            height: "30vh",
+                            width: "30vh"
+                        }}
+                    />
+                    <Pagination 
+                        count={props.product.images.length} 
+                        siblingCount={0} 
+                        page={page} 
+                        onChange={handlePage}
+                    />
+                </CardMedia>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                     {props.product.title}
