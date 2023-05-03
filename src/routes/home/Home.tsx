8fab4c3e150862c9bfbecc19b0959aba4e34910f
egunde <112527936+egunde@users.noreply.github.com>
2023-05-03 15:28:38 -0500

    improved home

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,18 +1,61 @@
 import { Box, Stack, Typography } from '@mui/material';
-import { styled, useTheme } from '@mui/material/styles';
+import { Theme, useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 
-const KitSelectionBox = styled('div')(({theme}) => ({
-    backgroundColor: theme.palette.primary.light,
-    height: 100,
-    width: 100,
-    border: '2px dashed',
-    borderRadius: 10,
-}))
+const useStyles = (theme: Theme) => ({
+    stepContainer: {
+        marginLeft:'200px',
+        marginTop: '-100px'
+    },
+    stepHeading: {
+        color: '#fff',
+        fontWeight:'bold',
+    },
+    stepStack: {
+        marginTop: '40px'
+    },
+    kitSelectionBox: {
+        height: '100px',
+        width: '100px',
+        border: '2px dashed',
+        borderRadius: 4,
+        backgroundColor: theme.palette.primary.main,
+    },
+    plus: {
+        display: 'flex',
+        color: '#fff',
+        fontWeight: 'bold',
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignContent: 'center',
+        marginTop: '4px'
+    }
+})
 
 export default function Home() {
-    const theme = useTheme();
+    const theme = useTheme()
+    const styles = useStyles(theme);
 
+    const KitSelectionBoxes = Array.from(Array(5).keys()).map((index) => {
+        return (
+            <Box 
+                key={index}
+                sx={styles.kitSelectionBox}
+            >
+                
+                <Link to='/catalog' style={{textDecoration: 'none'}}> 
+                <Typography 
+                    variant='h2'
+                    sx={styles.plus}
+                >   
+                    +
+                </Typography>
+                </Link>
+            </Box>
+        )
+    })
 
     return (
         <Stack>
@@ -22,30 +65,26 @@ export default function Home() {
                 }}
             >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill={theme.palette.primary.main} d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,149.3C840,160,960,192,1080,192C1200,192,1320,160,1380,144L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                <path fill={theme.palette.primary.dark} d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,149.3C840,160,960,192,1080,192C1200,192,1320,160,1380,144L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
             </svg>
             <Box
                 sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    height: '500px'
+                    backgroundColor: theme.palette.primary.dark,
+                    height: '500px',
                 }}
             >
-                <Typography variant='h1' color='#fff' ml='14vh'
-                    sx={{fontFamily: theme.typography.fontFamily}}
-                >
-                    Build Your Kit
-                </Typography>
-                <Stack direction='row' spacing={4} ml='14vh' mt='40px'>
-                    <KitSelectionBox></KitSelectionBox>
-                    <KitSelectionBox/>
-                    <KitSelectionBox/>
-                    <KitSelectionBox/>
-                    <KitSelectionBox/>
-                </Stack>
+                <Box sx={styles.stepContainer}>
+                    <Typography variant='h1' sx={styles.stepHeading}>
+                        Build Your Kit
+                    </Typography>
+                    <Stack direction='row' spacing={4} sx={styles.stepStack}>
+                        {KitSelectionBoxes}
+                    </Stack>
+                </Box>
             </Box>
 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill={theme.palette.primary.main} d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,149.3C840,160,960,192,1080,192C1200,192,1320,160,1380,144L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
+                <path fill={theme.palette.primary.dark} d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,149.3C840,160,960,192,1080,192C1200,192,1320,160,1380,144L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
             </svg>
             </Box>
         </Stack>
--- src/routes/root/Root.tsx
+++ src/routes/root/Root.tsx
@@ -1,10 +1,20 @@
-import { Box, ThemeProvider, createTheme, debounce } from '@mui/material'
+import { Box, ThemeProvider, createTheme, debounce, responsiveFontSizes } from '@mui/material'
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { store } from '../../store'
 import saveState, { loadState } from '../../utils/storage'
 import Navbar from './../layout/Navbar'
+import GoldmanSansWBdWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Bd.woff2'
+import GoldmanSansWBdItWoff2 from '../../utils/theming/fonts/GoldmanSans_W_BdIt.woff2'
+import GoldmanSansWBlkWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Blk.woff2'
+import GoldmanSansWItWoff2 from '../../utils/theming/fonts/GoldmanSans_W_It.woff2'
+import GoldmanSansWLtWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Lt.woff2'
+import GoldmanSansWMdWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Md.woff2'
+import GoldmanSansWMdItWoff2 from '../../utils/theming/fonts/GoldmanSans_W_MdIt.woff2'
 import GoldmanSansWRgWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Rg.woff2'
+import GoldmanSansWThWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Th.woff2'
+import GoldmanSansCdWBdWoff2 from '../../utils/theming/fonts/GoldmanSansCd_W_Bd.woff2'
+import GoldmanSansCdWRgWoff2 from '../../utils/theming/fonts/GoldmanSansCd_W_Rg.woff2'
 
 /*
 *   First element rendered by React Router
@@ -14,14 +24,28 @@ import GoldmanSansWRgWoff2 from '../../utils/theming/fonts/GoldmanSans_W_Rg.woff
 const theme = createTheme({
     palette: {
       primary: {
+        dark: '#6f00a8',
         main: '#a835ef',
+        light: '#f4d9ff'
       },
       secondary: {
         main: '#ffbb00',
       },
     },
     typography: {
-      fontFamily: GoldmanSansWRgWoff2,
+      fontFamily: [
+        GoldmanSansWBdWoff2,
+        GoldmanSansWBdItWoff2,
+        GoldmanSansWBlkWoff2,
+        GoldmanSansWItWoff2,
+        GoldmanSansWLtWoff2,
+        GoldmanSansWMdWoff2,
+        GoldmanSansWMdItWoff2,
+        GoldmanSansWRgWoff2,
+        GoldmanSansWThWoff2,
+        GoldmanSansCdWBdWoff2,
+        GoldmanSansCdWRgWoff2,
+        ].join(','),
     },
     components: {
       MuiCssBaseline: {
@@ -36,6 +60,7 @@ const theme = createTheme({
       }
     }
   });
+responsiveFontSizes(theme)
 
 store.subscribe(
     debounce(() => {
